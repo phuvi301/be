26b5738ccce868940e26aba80ca89c454c8fd98e
LMKgaming <leminh.khoa2907@gmail.com>
2025-10-19 00:50:37 +0700

    Fixed response structure

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 const verifyToken = (req, res, next) => {
     const token = req.headers.token;
-    if (!token) return res.status(401).json("You're not authenticated");
+    if (!token) return res.status(401).json({ message: "You're not authenticated" });
     const accessToken = token.split(" ")[1];
     jwt.verify(accessToken, process.env.JWT_ACCESS_KEY, (err, user) => {
         if (err) return res.status(403).json({ message: "Token is invalid" });
