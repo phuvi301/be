8898cf36457c4f701a995ffea174bf1133534da1
Vuxchan <luchanvu32123@gmail.com>
2025-12-19 13:15:31 +0700

    add recognition

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -162,9 +162,9 @@ const UserController = {
             tracks: tracks
         });
 
-    } catch (error) {
-        res.status(500).json({ message: "Internal server error", error });
-    }
+        } catch (error) {
+            res.status(500).json({ message: "Internal server error", error });
+        }
     },
 };
 
---
+++ src/service/shazamService.py
@@ -0,0 +1,32 @@
+import json
+import sys
+import requests
+import os
+from dotenv import load_dotenv
+
+url = "https://shazam-api6.p.rapidapi.com/shazam/recognize/"
+
+audio_data = sys.stdin.buffer.read()
+
+files = {
+    "upload_file": ("audio_file.mp3", audio_data, "audio/mpeg")
+}
+
+headers = {
+    "x-rapidapi-key": os.getenv("X_RAPIDAPI_KEY"), 
+    "x-rapidapi-host": "shazam-api6.p.rapidapi.com",
+}
+
+try:
+    response = requests.post(url, files=files, headers=headers)
+    response.raise_for_status()
+    result = response.json()
+
+    data = {
+        "title": result["track"]["title"],
+        "artist": result["track"]["subtitle"]
+    }
+    print(json.dumps(data))
+
+except Exception as e:
+    print(f"Python error: {str(e)}", file=sys.stderr)
