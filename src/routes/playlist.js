70a24ed705d5b9721e9f43ea3d13419ec685c4ee
Vinh <phuvi301@gmail.com>
2025-12-20 13:58:03 +0700

    add deleteImage middleware to deletePlaylist api

--- src/routes/playlist.js
+++ src/routes/playlist.js
@@ -5,9 +5,16 @@ import Playlists from "../models/Playlists.js";
 
 const router = express.Router();
 
+// Lấy thông tin playlist
 router.get("/:id", PlaylistsController.getPlaylist);
+
+// Tạo playlist mới
 router.post("/", middlewareController.verifyToken, PlaylistsController.createPlaylist);
+
+// Cập nhật thông tin playlist (không bao gồm ảnh thumbnail) 
 router.put("/:id", middlewareController.verifyToken, PlaylistsController.updatePlaylist);
+
+// Cập nhật ảnh thumbnail của playlist trên Cloudinary
 router.put(
     "/:id/thumbnail",
     middlewareController.verifyToken,
@@ -16,7 +23,17 @@ router.put(
     middlewareController.handleThumbnailUpdate,
     PlaylistsController.updateThumbnail
 );
-router.delete("/:id", middlewareController.verifyToken, PlaylistsController.deletePlaylist);
+
+// Xoá playlist cùng với ảnh thumbnail trên Cloudinary nếu có
+router.delete(
+    "/:id",
+    middlewareController.verifyToken,
+    middlewareController.verifyOwner(Playlists),
+    middlewareController.handleThumbnailUpdate,
+    PlaylistsController.deletePlaylist
+);
+
+// Thêm hoặc xoá track khỏi playlist
 router.post("/:id/add", middlewareController.verifyToken, PlaylistsController.addTrackToPlaylist);
 router.delete("/:id/remove", middlewareController.verifyToken, PlaylistsController.removeTrackFromPlaylist);
 
