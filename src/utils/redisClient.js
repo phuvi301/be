2805b2f3486b55cb7324f589e0994c8283b1cd59
Vinh <phuvi301@gmail.com>
2026-04-16 19:27:30 +0700

    modify Redis connection

--- src/utils/redisClient.js
+++ src/utils/redisClient.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import Redis from "ioredis"
 
 
-const redis = new Redis(`rediss://default:${process.env.REDIS_KEY}@trusting-javelin-85767.upstash.io:6379`);
+const redis = new Redis(`${process.env.REDIS_URL}`);
 redis.on("connect", () => console.log("✅ Redis connected"));
 redis.on("error", (err) => console.error("❌ Redis error:", err));
 
